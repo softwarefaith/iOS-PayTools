af771b2b18cc9a26e71b717fd985566396e0c067
Alan <softwarefaith@126,con>
2016-12-30 12:10:51 +0800

    iOS -PayTool

---
+++ AllPayDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AllPayDemo
+//
+//  Created by Allan on 16/10/24.
+//  Copyright © 2016年 Allan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
